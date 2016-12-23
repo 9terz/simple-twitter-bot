c0758df571f67cbbe497bccd5c9c02050d41f8ee
9terz <9terz666@gmail.com>
2016-12-23 21:56:42 +0700

    edit word to tweet

--- tests.py
+++ tests.py
@@ -24,13 +24,21 @@ while(1):
     soup = BeautifulSoup(optionsPage,"html.parser")
     current_date = time.strftime("%a %d %b %H:%M")
     strtodel = ["]","</p>"]
+    strtodel2 = ["<strong>","</strong>"]
     wordtosay = str(soup.find_all('p',{'id':'update-at'},'.'))[51:].translate(None,"".join(strtodel)).decode('unicode-escape')
- 
     word = wordtosay.encode('utf-8')
-    words = "ขณะนี้เวลา " + current_date + " ในเวปดูเกรดขึ้นว่า  "+word +" ดูได้ที่ https://www3.reg.cmu.ac.th/transcript/"
-    print len(words)
-    api.update_status(status=words)
+    words = current_date + " ในเวปดูเกรดขึ้นว่า  "+word +" ดูได้ที่ https://www3.reg.cmu.ac.th/transcript/"
+    onlineuser = str(soup.find_all('div',{'id':'useronline'},'.'))[22:-7].translate(None,"".join(strtodel2)).decode('unicode-escape')
+    online = onlineuser.encode('utf-8')
+    if(word == 'ข้อมูลอัพเดทเมื่อวันที่ 23 ธันวาคม 2559'):
+    	api.update_status(status='เฮ้ย อัพเดทวันที่ 23 เกรดออกแล้วปะวะสาสสสสสส ไปเช็คดิ๊ https://www3.reg.cmu.ac.th/transcript/')
+    elif(word == 'ข้อมูลอัพเดทเมื่อวันที่ 24 ธันวาคม 2559'):
+    	api.update_status(status='เฮ้ย อัพเดทวันที่ 24 เกรดออกแล้วปะวะสาสสสสสส ไปเช็คดิ๊ https://www3.reg.cmu.ac.th/transcript/')
+    else:
+    	tweet = online + ' แต่เกรดยังม่ายออก'
+    	api.update_status(status=tweet)
     print word
+    print online
     time.sleep(444)
 #api.update_status(status=urlEncodeNonAscii(cname))
 #print cname
